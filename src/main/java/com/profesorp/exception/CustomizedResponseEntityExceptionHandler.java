0bfb656a442477e864d01c58ec8bb1058f8ab54b
Jesus Javier Puente <chuchip@gmail.com>
2018-12-02 12:09:18 +0100

    Sustituido @ControllerAdvice and @RestController  por @RestControllerAdvice. Thanks to Marcelo Martins

--- src/main/java/com/profesorp/exception/CustomizedResponseEntityExceptionHandler.java
+++ src/main/java/com/profesorp/exception/CustomizedResponseEntityExceptionHandler.java
@@ -4,15 +4,14 @@ import java.util.Date;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
-
-@ControllerAdvice
-@RestController
+@RestControllerAdvice
+//@ControllerAdvice
+//@RestController
 public class CustomizedResponseEntityExceptionHandler extends ResponseEntityExceptionHandler {
 
 	@ExceptionHandler(BeanNotFoundException.class)
